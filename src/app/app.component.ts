1e2fe01fa992f1a5b2df808a6fa043b6c0ed220a
Rafael de Jesus Carvalho <141766102+rafaeldjcarvalho@users.noreply.github.com>
2024-10-26 16:19:12 -0300

    Primeiro Hello world

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MeuPrimeiro2Component } from "./meu-primeiro2/meu-primeiro2.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MeuPrimeiroComponent, MeuPrimeiro2Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'primeiro-projeto';
+  title = 'Olá Mundo!';
 }
---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'meu-primeiro-component',
+  standalone: true,
+  template: '<p>Meu primeiro componente com Angular!</p>'
+})
+export class MeuPrimeiroComponent {}
